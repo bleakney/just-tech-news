71aba6be67531da39de091627510b875777da4d1
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-02 16:32:44 -0500

    install handlebars and set up in server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
