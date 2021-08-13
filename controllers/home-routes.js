bfe9d91faae81d40ac3bc65c6c5d9459e142d142
Bleakney Bissett <bleakneybissett@gmail.com>
2021-08-13 17:06:21 -0500

    conditionally render commenting

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,7 +38,9 @@ router.get('/', (req, res) => {
       .then(dbPostData => {
           const posts = dbPostData.map(post => post.get({ plain: true }));
         // pass a single post object into the homepage template
-        res.render('homepage', { posts });
+        res.render('homepage', { 
+        posts,
+        loggedIn: req.session.loggedIn });
       })
       .catch(err => {
         console.log(err);
@@ -83,7 +85,7 @@ router.get('/', (req, res) => {
           const post = dbPostData.get({ plain: true });
 
           // pass data to template
-          res.render('single-post', { post });
+          res.render('single-post', { post, loggedIn: req.session.loggedIn });
 
       })
       .catch(err => {
